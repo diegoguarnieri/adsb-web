9d81bd11b56cd5f901e2ea36edd145736611e987
diegoguarnieri <diego@Diegos-MacBook-Air.local>
2019-09-02 15:14:59 -0300

    .

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY','aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'),
 
     'cipher' => 'AES-256-CBC',
 
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::post('adsb', 'AdsbController@store');
